4ead89a559f39497df3c8c49f5b13dea53233045
Mahmoud Al-Haroon <mahmoud.harooney.tooling@gmail.com>
2020-12-31 18:12:15 +0200

    Grid item widget folder uncommented and refctored

--- lib/widgets/grid_item.dart
+++ lib/widgets/grid_item.dart
@@ -3,25 +3,25 @@ import 'package:flutter/material.dart';
 import '../viewobject/product.dart';
 
 class SearchItemItem extends StatelessWidget {
-  // final String itemOneImg;
-  // final String itemOneTitle;
-  // final Color itemOneBgColor;
-  // final Color itemOneTextColor;
-  // final String itemTwoImg;
-  // final String itemTwoTitle;
-  // final Color itemTwoBgColor;
-  // final Color itemTwoTextColor;
-  // SearchItemItem({
-  //   Key key,
-  //   this.itemOneImg,
-  //   this.itemOneTitle,
-  //   this.itemOneBgColor,
-  //   this.itemOneTextColor,
-  //   this.itemTwoImg,
-  //   this.itemTwoTitle,
-  //   this.itemTwoBgColor,
-  //   this.itemTwoTextColor,
-  // }) : super(key: key);
+  final String itemOneImg;
+  final String itemOneTitle;
+  final Color itemOneBgColor;
+  final Color itemOneTextColor;
+  final String itemTwoImg;
+  final String itemTwoTitle;
+  final Color itemTwoBgColor;
+  final Color itemTwoTextColor;
+  SearchItemItem({
+    Key key,
+    this.itemOneImg,
+    this.itemOneTitle,
+    this.itemOneBgColor,
+    this.itemOneTextColor,
+    this.itemTwoImg,
+    this.itemTwoTitle,
+    this.itemTwoBgColor,
+    this.itemTwoTextColor,
+  }) : super(key: key);
 
   final List<Product> loadedProduct = [
     Product(
@@ -34,111 +34,95 @@ class SearchItemItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Padding(
-    //   padding: const EdgeInsets.only(bottom: 40),
-    //   child: Row(
-    //     children: <Widget>[
-    //       Flexible(
-    //           child: Container(
-    //         width: double.infinity,
-    //         height: 140,
-    //         child: Stack(
-    //           children: <Widget>[
-    //             Align(
-    //               alignment: Alignment.bottomCenter,
-    //               child: Container(
-    //                 width: double.infinity,
-    //                 height: 100,
-    //                 decoration: BoxDecoration(
-    //                     color: itemOneBgColor,
-    //                     borderRadius: BorderRadius.circular(15)),
-    //                 child: Align(
-    //                   alignment: Alignment.centerLeft,
-    //                   child: Padding(
-    //                     padding: const EdgeInsets.only(left: 12),
-    //                     child: Text(
-    //                       itemOneTitle,
-    //                       style: TextStyle(
-    //                           fontSize: 13,
-    //                           height: 1.5,
-    //                           color: itemOneTextColor),
-    //                     ),
-    //                   ),
-    //                 ),
-    //               ),
-    //             ),
-    //             Positioned(
-    //               right: -20,
-    //               top: -10,
-    //               child: Align(
-    //                 alignment: Alignment.topRight,
-    //                 child: Image.asset(itemOneImg),
-    //               ),
-    //             )
-    //           ],
-    //         ),
-    //       )),
-    //       SizedBox(
-    //         width: 25,
-    //       ),
-    //       Flexible(
-    //           child: Container(
-    //         width: double.infinity,
-    //         height: 140,
-    //         child: Stack(
-    //           children: <Widget>[
-    //             Align(
-    //               alignment: Alignment.bottomCenter,
-    //               child: Container(
-    //                 width: double.infinity,
-    //                 height: 100,
-    //                 decoration: BoxDecoration(
-    //                     color: itemTwoBgColor,
-    //                     borderRadius: BorderRadius.circular(15)),
-    //                 child: Align(
-    //                   alignment: Alignment.centerLeft,
-    //                   child: Padding(
-    //                     padding: const EdgeInsets.only(left: 12),
-    //                     child: Text(
-    //                       itemTwoTitle,
-    //                       style: TextStyle(
-    //                           fontSize: 13,
-    //                           height: 1.5,
-    //                           color: itemTwoTextColor),
-    //                     ),
-    //                   ),
-    //                 ),
-    //               ),
-    //             ),
-    //             Positioned(
-    //               right: -20,
-    //               top: -10,
-    //               child: Align(
-    //                 alignment: Alignment.topRight,
-    //                 child: Image.asset(itemTwoImg),
-    //               ),
-    //             )
-    //           ],
-    //         ),
-    //       )),
-    //     ],
-    //   ),
-    // );
-    // return GridView.builder(
-    //   padding: const EdgeInsets.only(bottom: 40),
-    //   itemCount: loadedProduct.length,
-    //   itemBuilder: (ctx, i) => Product(
-    //     loadedProduct[i].productId,
-    //     loadedProduct[i].productTitle,
-    //     loadedProduct[i].productImage,
-    //     loadedProduct[i].price,
-    //   ),
-    //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-    //     crossAxisCount: 2,
-    //     childAspectRatio: 3 / 2,
-    //     crossAxisSpacing: 10,
-    //     mainAxisSpacing: 10,
-    //   ),
-    // );
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 40),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+              child: Container(
+            width: double.infinity,
+            height: 140,
+            child: Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        color: itemOneBgColor,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 12),
+                        child: Text(
+                          itemOneTitle,
+                          style: TextStyle(
+                              fontSize: 13,
+                              height: 1.5,
+                              color: itemOneTextColor),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: -20,
+                  top: -10,
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Image.asset(itemOneImg),
+                  ),
+                )
+              ],
+            ),
+          )),
+          SizedBox(
+            width: 25,
+          ),
+          Flexible(
+              child: Container(
+            width: double.infinity,
+            height: 140,
+            child: Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        color: itemTwoBgColor,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 12),
+                        child: Text(
+                          itemTwoTitle,
+                          style: TextStyle(
+                              fontSize: 13,
+                              height: 1.5,
+                              color: itemTwoTextColor),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: -20,
+                  top: -10,
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Image.asset(itemTwoImg),
+                  ),
+                )
+              ],
+            ),
+          )),
+        ],
+      ),
+    );
   }
 }
