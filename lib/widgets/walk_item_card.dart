a2291385ccf75452fde3026a0d76a3a0b120cef7
Mahmoud Al-Haroon <mahmoud.harooney.tooling@gmail.com>
2021-01-02 23:50:39 +0200

    walk item card widget added

--- lib/widgets/walk_item_card.dart
+++ lib/widgets/walk_item_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import '../constants/theme.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ipetfe/constants/i_pet_constants.dart';
+
+import '../constants/theme.dart';
 
 class WalkGroupCard extends StatelessWidget {
   final String imgScr;
@@ -9,19 +11,28 @@ class WalkGroupCard extends StatelessWidget {
   final String members;
   final String orgBy;
   const WalkGroupCard({
-    Key key, this.imgScr, this.title, this.location, this.members, this.orgBy,
+    Key key,
+    this.imgScr,
+    this.title,
+    this.location,
+    this.members,
+    this.orgBy,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 30,bottom: 10),
+      padding: const EdgeInsets.only(right: 30, bottom: 10),
       child: Container(
         decoration: BoxDecoration(
-            color: textWhite,
+            color: AppTheme.white,
             borderRadius: BorderRadius.circular(15),
-            boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.3),blurRadius: 8,offset: Offset(3, 1))]
-        ),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  blurRadius: 8,
+                  offset: Offset(3, 1))
+            ]),
         child: Container(
           width: 280,
           child: Column(
@@ -31,45 +42,79 @@ class WalkGroupCard extends StatelessWidget {
                 padding: EdgeInsets.all(18),
                 child: Column(
                   children: <Widget>[
-                    Text(title,style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,height: 1.5),),
-                    SizedBox(height: 10,),
+                    Text(
+                      title,
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          height: 1.5),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
                     Row(
                       children: <Widget>[
-                        Icon(FontAwesomeIcons.mapMarkerAlt,size: 17,),
-                        SizedBox(width: 15,),
-                        Text(location,style: contentBlack,)
+                        Icon(
+                          FontAwesomeIcons.mapMarkerAlt,
+                          size: 17,
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          location,
+                          style: IPetConst.contentBlack,
+                        )
                       ],
                     ),
-                    SizedBox(height: 10,),
+                    SizedBox(
+                      height: 10,
+                    ),
                     Row(
                       children: <Widget>[
-                        Icon(FontAwesomeIcons.users,size: 17,),
-                        SizedBox(width: 15,),
-                        Text(members,style: contentBlack,)
+                        Icon(
+                          FontAwesomeIcons.users,
+                          size: 17,
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          members,
+                          style: IPetConst.contentBlack,
+                        )
                       ],
                     ),
-                    SizedBox(height: 10,),
+                    SizedBox(
+                      height: 10,
+                    ),
                     Row(
                       children: <Widget>[
-                        Icon(Icons.account_circle,size: 20,),
-                        SizedBox(width: 15,),
-                        RichText(text:
-                        TextSpan(
-                            text: "Organized by ",
-                            style: contentBlack,
-                            children: <TextSpan>[
-                              TextSpan(text: orgBy,style: TextStyle(color: primary)),
-                            ]
-                        )
-                        )
+                        Icon(
+                          Icons.account_circle,
+                          size: 20,
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        RichText(
+                            text: TextSpan(
+                                text: "Organized by ",
+                                style: IPetConst.contentBlack,
+                                children: <TextSpan>[
+                              TextSpan(
+                                  text: orgBy,
+                                  style: TextStyle(color: AppTheme.nearlyBlue)),
+                            ]))
                         // Text("Organized by Laura ",style: contentBlack,)
                       ],
                     ),
-                    SizedBox(height: 10,)
+                    SizedBox(
+                      height: 10,
+                    )
                   ],
                 ),
               ),
-
             ],
           ),
         ),
